aa56ec379e999debe9eac6de508e5a0f325cae8a
Fellipe-Aparecido <fellipeappereirasilva@gmail.com>
2023-04-05 16:32:39 -0300

    correção escrita do conteúdo de uma das constantes

--- src/main/java/br/com/rest/api/util/MediaType.java
+++ src/main/java/br/com/rest/api/util/MediaType.java
@@ -2,7 +2,7 @@ package br.com.rest.api.util;
 
 public class MediaType {
 
-    public static final String APPLICATION_JSON = "application/x-json";
+    public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_XML = "application/xml";
     public static final String APPLICATION_YML = "application/x-yaml";
 }

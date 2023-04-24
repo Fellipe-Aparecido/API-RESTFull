0b51aec449c0e5e00d7076a374afb7b4e4d77e03
Fellipe-Aparecido <fellipeappereirasilva@gmail.com>
2023-04-24 15:15:31 -0300

    Implementando novo endpoint para Books

---
+++ src/main/java/br/com/rest/api/data/model/Book.java
@@ -0,0 +1,79 @@
+package br.com.rest.api.data.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "launch_date")
+    private Date launchDate;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "title")
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(getId(), book.getId()) && Objects.equals(getAuthor(), book.getAuthor()) && Objects.equals(getLaunchDate(), book.getLaunchDate()) && Objects.equals(getPrice(), book.getPrice()) && Objects.equals(getTitle(), book.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getAuthor(), getLaunchDate(), getPrice(), getTitle());
+    }
+}
---
+++ src/main/java/br/com/rest/api/services/BookServices.java
@@ -0,0 +1,69 @@
+package br.com.rest.api.services;
+
+import br.com.rest.api.controller.BookController;
+import br.com.rest.api.converter.DozerConverter;
+import br.com.rest.api.data.model.Book;
+import br.com.rest.api.data.vo.v1.BookVO;
+import br.com.rest.api.exception.RequiredObjectIsNullException;
+import br.com.rest.api.exception.ResourceNotFoundException;
+import br.com.rest.api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class BookServices {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookVO create(BookVO bookVO) {
+        if (bookVO == null) throw new RequiredObjectIsNullException();
+        var entity = DozerConverter.parseObject(bookVO, Book.class);
+        var vo = DozerConverter.parseObject(bookRepository.save(entity), BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class)
+                .findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public List<BookVO> findAll() {
+        var books = DozerConverter.parseListObjects(bookRepository.findAll(), BookVO.class);
+        books.stream().forEach(b-> b.add(linkTo(methodOn(BookController.class).findById(b.getKey())).withSelfRel()));
+        return books;
+    }
+
+    public BookVO findById(Long id) {
+
+        var entity = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        var vo = DozerConverter.parseObject(entity, BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+        return vo;
+    }
+
+    public BookVO update(BookVO book) {
+        if (book == null) throw new RequiredObjectIsNullException();
+        var entity = bookRepository.findById(book.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        entity.setAuthor(book.getAuthor());
+        entity.setLaunchDate(book.getLaunchDate());
+        entity.setPrice(book.getPrice());
+        entity.setTitle(book.getTitle());
+
+        var vo = DozerConverter.parseObject(bookRepository.save(entity), BookVO.class);
+        vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+        return vo;
+    }
+
+    public void delete(Long id) {
+        Book entity = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        bookRepository.delete(entity);
+    }
+
+}
